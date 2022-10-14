85e1b68bf6041e22b6a8ef39fc44d26fea91eb3c
Kuby <kuby@cocho.io>
2022-10-15 01:27:21 +0900

    add opanapi assets

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
 
@@ -16,6 +18,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  app.useStaticAssets(path.join(__dirname, '/static'), {
+    prefix: '/docs',
+  });
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
